5d0443d753dae38d4eaaa92721eccc9099242e92
Zeba <saidzodazebida@gmail.com>
2025-12-03 20:09:23 +0300

    third

--- app/src/main/java/com/example/superheroes/ui/theme/Shape.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Shape.kt
@@ -4,4 +4,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes()
+val Shapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(16.dp)
+)
