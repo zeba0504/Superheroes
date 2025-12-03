7ed88c0c60d530e9db9bfb2198e8d12748d050eb
Zeba <saidzodazebida@gmail.com>
2025-12-03 20:08:58 +0300

    second

---
+++ app/src/main/java/com/example/superheroes/ui/theme/Shape.kt
@@ -0,0 +1,7 @@
+package com.example.superheroes.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes()
