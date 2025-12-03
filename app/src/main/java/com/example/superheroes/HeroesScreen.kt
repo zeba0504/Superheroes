d896202bd069bc98d4b67bfc502ee7ed65534630
Zeba <saidzodazebida@gmail.com>
2025-12-03 20:23:27 +0300

    seventh

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -39,9 +39,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.sizeIn
 import com.example.superheroes.model.Hero
 import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -87,20 +93,33 @@ fun HeroesList(
     }
 }
 
-
-
-
 @Composable
 fun HeroListItem(
     hero: Hero,
     modifier: Modifier = Modifier
-) {
-    Card(
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        modifier = modifier,
     ) {
-
-    }
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+            modifier = modifier,
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .sizeIn(minHeight = 72.dp)
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = stringResource(hero.nameRes),
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                    Text(
+                        text = stringResource(hero.descriptionRes),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
 }
 
 @Preview("Light Theme")
